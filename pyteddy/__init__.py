bfffd4f57ec7440ecc0d4877682a323e3b8649a0
Robert Spralja <robert.spralja@gmail.com>
2022-03-10 13:27:43 +0100

    restructured global keyword args

--- pyteddy/__init__.py
+++ pyteddy/__init__.py
@@ -1,4 +1,2 @@
 
 __version__ = '0.1.0'
-
-CARGS = 'verbose',
--- pyteddy/__main__.py
+++ pyteddy/__main__.py
@@ -1,4 +1,4 @@
-from . import _db, CARGS
+from . import _db, __version__
 from ._parser import parser
 
 import sys
@@ -16,22 +16,18 @@ def execute(kwargs):
 def main():
     kwargs = vars(parser.parse_args())
     
-    for garg in gargs:
-        gkwargs[garg] = kwargs.pop(garg)
-        if garg in CARGS:
-            carg = _db.get('config').get(garg)
-            if carg:
-                gkwargs[garg] = carg
+    verbose = kwargs.pop('verbose')
+
+    if kwargs.pop('version'):
+        print(f'v{__version__}')
+        exit()
+
     try: 
         execute(kwargs)
     except Exception as e:
         print('Error: an error has accoured')
-        if gkwargs['verbose']:
+        if kwargs['verbose']:
             traceback.print_exception(e, e, sys.exc_info()[2])
-            if gkwargs['verbose'] is True:
-                print('remove --verbose to supress printing python stack trace')
-            else:
-                print(f"python stack trace is printing because verbose is set to {gkwargs['verbose']!r} in config,\nrun \'pyteddy config del verbose\' to remove it from config")
         else:
             print('add --verbose to print python stack trace')
 
